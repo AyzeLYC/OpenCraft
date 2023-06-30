d971de5b27b60ce78c46060f0c58441577df33cd
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-06-30 09:16:12 +0200

    Updated main.java

--- src/main.java
+++ src/main.java
@@ -5,11 +5,20 @@ import java.lang.Thread;
 
 public class Main {
 
+    private boolean gameInPause = false;
     private boolean playing = true;
     
     while (playing == true) {
         
-        
+        if (gameInPause == true) {
+            
+            
+            
+        } else {
+            
+            
+            
+        };
         
     };
     
