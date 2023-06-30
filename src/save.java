29e2c2b6c09978a83b06837c21b4c4fa8ba43040
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-06-30 09:22:31 +0200

    Created save.java

---
+++ src/save.java
@@ -0,0 +1,30 @@
+package save;
+
+import java.io.File;
+
+public class Save {
+
+    public String GAMEDATAS {
+        
+        public String SAVENAME;
+        public String GAMEVERSION;
+        public PLAYER PLAYERS[];
+        public String COMPANIES;
+        public MAP SAVEMAP[];
+        public OBJECT OBJECTS[];
+        public TEXT TEXTS[];
+        
+    }
+
+    public void readGameFile(String SaveDirectory, String SaveName) {
+        
+        
+        
+    };
+    public void saveGame(GAMEDATAS GameDatas) {
+        
+        
+        
+    }
+    
+}
