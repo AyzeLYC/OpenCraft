6b95fa39d30d5d53ed624c5370695b4015a8a736
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-06-30 15:37:34 +0200

    Created npc.java

---
+++ src/npc.java
@@ -0,0 +1,19 @@
+package npc;
+
+import java.awt.Color;
+
+public class NPC {
+
+    String NPCs[];
+    
+    public void createNpc(String name, ) {
+        
+      
+    };
+    public void createNpcWalkingPath(String XPosition, String YPosition, String ZPosition, String speed) {
+
+
+      
+    };
+    
+}
